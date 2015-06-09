9cd26fdb7485635621067520efc7b0bd60c13d31
gwalker1228 <gwalker1228@gmail.com>
2015-06-09 11:40:02 -0700

    Initial Commit

---
+++ Instagram2.0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instagram2.0
+//
+//  Created by Gretchen Walker on 6/9/15.
+//  Copyright (c) 2015 Gretchen Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
